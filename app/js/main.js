81008f67f77dca8b81dfe8954c756bdcd82711bf
DisturbedMild <v.dmitrenko1998@gmail.com>
2019-07-21 19:04:11 +0300

    added complex,contacts blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function(){
+  $('.production__slider').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1.04,
+    slidesToScroll: 1,
+    speed: 700,
+    infinite:false,
+    variableWidth: true,
+  });
+  $(".production__slider").on('afterChange', function(event, slick, currentSlide){
+    $(".production__num").text(currentSlide + 1);
+ });
 
+ $('.complex__slider').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 700,
+  infinite:false,
+  rtl:true,
+  variableWidth: true,
+});
+$(".complex__slider").on('afterChange', function(event, slick, currentSlide){
+  $(".complex__num").text(currentSlide + 1);
+});
 });
