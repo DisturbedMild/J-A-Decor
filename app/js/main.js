86a6364c2ad00463993d6ef8e351716c635e00dd
DisturbedMild <v.dmitrenko1998@gmail.com>
2019-08-31 16:27:15 +0300

    changed 2nd slider,added full adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,41 @@
 $(function(){
   $('.production__slider').slick({
-    lazyLoad: 'ondemand',
-    slidesToShow: 1.04,
+    lazyLoad: 'progressive',
+    slidesToShow: 1,
     slidesToScroll: 1,
     speed: 700,
     infinite:false,
     variableWidth: true,
+    responsive: [
+      { 
+        breakpoint: 1280,
+        settings: {
+          variableWidth:false,
+          centerPadding: '40px',
+        }
+      }
+    ]
   });
   $(".production__slider").on('afterChange', function(event, slick, currentSlide){
     $(".production__num").text(currentSlide + 1);
  });
 
  $('.complex__slider').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  speed: 700,
-  infinite:false,
-  rtl:true,
-  variableWidth: true,
+ lazyLoad: 'progressive',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 700,
+    infinite:false,
+    variableWidth: true,
+    responsive: [
+      { 
+        breakpoint: 1280,
+        settings: {
+          variableWidth:false,
+          centerPadding: '40px',
+        }
+      }
+    ]
 });
 $(".complex__slider").on('afterChange', function(event, slick, currentSlide){
   $(".complex__num").text(currentSlide + 1);
